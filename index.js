a5a524db79feb28f89d66a165c1f673a6f566cfa
Samih Mustafa <samihmustafa@gmail.com>
2024-09-03 19:19:54 +0400

    phonebook backend 3.16 complete - add errorhandling middleware

--- index.js
+++ index.js
@@ -15,66 +15,32 @@ morgan.token('post-body', (req) => {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
-    })
+    }).catch(error => next(error))
   })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    if (person) {
+    
+    Person.findById(id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
     const id = request.params.id
     Person.findByIdAndDelete(id).then(() => {
         response.status(204).end()
-    }).catch(error => {
-        console.log(error)
-        response.status(404).end()
-    })
+    }).catch(error => next(error))
 })
 
-
-const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
-  }
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -86,7 +52,7 @@ app.post('/api/persons', (request, response) => {
 
     person.save().then(person => {
         response.json(person)
-    })
+    }).catch(error => next(error))
   })
 
 
@@ -96,6 +62,19 @@ app.post('/api/persons', (request, response) => {
 
 app.use(unknownEndpoint)
 
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  // this has to be the last loaded middleware, also all the routes should be registered before this!
+  app.use(errorHandler)
 
 
 const PORT = process.env.PORT
