6ec70d41b1d7599cc2060d08c84ca62cc48f8620
Samih Mustafa <samihmustafa@gmail.com>
2024-09-05 12:06:09 +0400

    blog 4.5 complete - add favorite blog test

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,6 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((a, b) => {
+    return (a.likes > b.likes) ? a : b
+  }, blogs.length === 0 ? undefined : blogs[0])
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
